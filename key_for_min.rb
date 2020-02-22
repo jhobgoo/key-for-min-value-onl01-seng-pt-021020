af7892b183cc825989b4fd3ee4c84989b5785557
Joel Hobgood <jhobgoo@gmail.com>
2020-02-22 05:42:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest.value
-    lowest_key == key
-    lowest_value == value
+    lowest_key == k
+    lowest_value == v
     end
   end
   lowest_key
