6d807cf9e354f09b6da0c20db4f5cf3917d19307
Joel Hobgood <jhobgoo@gmail.com>
2020-02-22 05:31:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |key, value|
+    if lowest_value == nil || value < lowest.value
+      lowest_value == value
+      lowest_key == key
+    end
+  end
+  lowest_key
 end
